2bc3b67c547f3eb888d50fa4aaa903a8c1981bb9
Igor Amaral <igoramaraal089@gmail.com>
2025-11-11 10:21:13 -0300

    Fix: Abrindo o server.js para escutar na rede.

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,7 @@
 
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server rodando na porta ${PORT}`);
 });
